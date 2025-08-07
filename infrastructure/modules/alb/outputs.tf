ffdb19d0a48bc46b5070d71e7b1df4b7e7e24b60
Durlabh8972 <dtilavat8972@conestogac.on.ca>
2025-08-07 04:14:11 +0300

    Terraform iac (#7)
    - Included test section to support running tests
    - Included a build section that depends on test and security scan to build the docker containers.
    
    * Added terraform IAC and included supporting configuration to support auto builds.
    
    * Updates to github secrets.
    
    * --Completed setting up aws instances, configured all terraform files to meet the current build requirements.
    
    --Added more changes to the docker compose and ci-cd.yml file.

---
+++ infrastructure/modules/alb/outputs.tf
@@ -0,0 +1,21 @@
+# infrastructure/modules/alb/outputs.tf
+
+output "dns_name" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.main.dns_name
+}
+
+output "zone_id" {
+  description = "Zone ID of the load balancer"
+  value       = aws_lb.main.zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.app.arn
+}
+
+output "load_balancer_arn" {
+  description = "ARN of the load balancer"
+  value       = aws_lb.main.arn
+}
