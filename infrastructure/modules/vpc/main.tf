8c2ba014300ac0e60104699aa90f215d044e85ce
Durlabh8972 <dtilavat8972@conestogac.on.ca>
2025-08-12 19:16:04 +0300

    update

--- infrastructure/modules/vpc/main.tf
+++ infrastructure/modules/vpc/main.tf
@@ -1,13 +1,18 @@
 # infrastructure/modules/vpc/main.tf
-resource "aws_vpc" "main" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
 
-  tags = {
-    Name = "${var.environment}-vpc"
-  }
+data "aws_vpc" "existing" {
+  id = "vpc-01485c542f2e978aa"
 }
+
+# resource "aws_vpc" "main" {
+#   cidr_block           = var.vpc_cidr
+#   enable_dns_hostnames = true
+#   enable_dns_support   = true
+
+#   tags = {
+#     Name = "${var.environment}-vpc"
+#   }
+# }
 
 resource "aws_internet_gateway" "main" {
   vpc_id = aws_vpc.main.id
@@ -20,7 +25,8 @@ resource "aws_internet_gateway" "main" {
 resource "aws_subnet" "public" {
   count = length(var.availability_zones)
 
-  vpc_id                  = aws_vpc.main.id
+  # vpc_id                  = aws_vpc.main.id
+  vpc_id                  = data.aws_vpc.existing.id  # Use existing VPC
   cidr_block              = cidrsubnet(var.vpc_cidr, 8, count.index)
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
