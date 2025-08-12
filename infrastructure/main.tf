aaf732a0d166bf243cb9c041b150a11cd03c7af0
Durlabh8972 <dtilavat8972@conestogac.on.ca>
2025-08-12 23:08:19 +0300

    Update AWS AMI filter to use Ubuntu 20.04 and add virtualization type filter

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,16 +26,29 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+#   # owners      = ["099720109477"] # Canonical
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+#   }
+# }
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners      = ["099720109477"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
